1f25abdf48a239260402e827802aaa78690b6d02
chalgul <129812127+chalgul@users.noreply.github.com>
2025-09-29 10:32:34 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 22.67 ms, Memory: 23.5 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
 
 ### 성능 요약
 
-메모리: 23.5 MB, 시간: 20.23 ms
+메모리: 23.5 MB, 시간: 22.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 27일 16:58:03
+2025년 09월 29일 10:32:27
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
@@ -8,47 +8,45 @@ int solution(vector<int> diffs, vector<int> times, long long limit) {
     int answer = 0;
 
     // 1. 이분 탐색 범위 설정
-    int start = 1;
-    int end = *max_element(diffs.begin(), diffs.end());
+    int Start = 1;
+    int End = *max_element(diffs.begin(), diffs.end());
+    int Mid = (Start + End)/2;
     
-    // 2. 이분 탐색 시작
-    while (start <= end) {
-        // 2-1. 매번 새로운 mid(테스트할 숙련도) 값을 계산
-        int mid = start + (end - start) / 2;
+    
+    while(End >= Start)
+    {
+        long long TotalSpendingTime = 0;
         
-        long long totalSpendingTime = 0;
-
-        // 2-2. 'mid' 숙련도로 모든 퍼즐을 풀 때의 총 시간을 계산
-        for (size_t i = 0; i < diffs.size(); ++i) {
-            // 기본 소요 시간은 항상 추가됨
-            totalSpendingTime += times[i];
+        int PuzzleSize = (int)diffs.size();
+        
+        for(int i = 0; i < PuzzleSize; ++i)
+        {
+            TotalSpendingTime += times[i];
             
-            // 숙련도가 부족할 경우 추가 시간 계산
-            if (diffs[i] > mid) {
-                long long failures = diffs[i] - mid;
-                long long penalty_per_failure = 0;
-
-                penalty_per_failure = times[i] + times[i-1];
-                
-                totalSpendingTime += failures * penalty_per_failure;
+            //퍼즐의 레벨이 더 높은 경우
+            if(diffs[i] > Mid)
+            {
+                //첫번째 퍼즐의 레벨은 1 고정이라서 -1인덱스 접근 예외처리 필요X 
+                TotalSpendingTime += (diffs[i] - Mid)*(times[i-1]+times[i]);
             }
             
-            // 최적화: 계산 중간에 이미 limit을 넘었다면 더 볼 필요 없이 중단
-            if (totalSpendingTime > limit) {
+            
+            if(TotalSpendingTime > limit)
+            {
+                Start = Mid + 1;
                 break;
             }
         }
-
-        // 2-3. 계산된 총 시간과 limit을 비교하여 범위 조정
-        if (totalSpendingTime <= limit) {
-            // 성공: 이 레벨(mid)은 가능. 더 낮은 최소 레벨을 찾기 위해 범위를 아래로 좁힘
-            answer = mid;
-            end = mid - 1;
-        } else {
-            // 실패: 이 레벨(mid)은 불가능. 더 높은 레벨이 필요하므로 범위를 위로 좁힘
-            start = mid + 1;
+        
+        if(TotalSpendingTime <= limit)
+        {
+            answer = Mid;
+            End = Mid - 1;
         }
+        
+        Mid = (Start + End) / 2 ;
     }
+    
 
     return answer;
 }
